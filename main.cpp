12ea8bad3a7464d184ddb78bb05b5b988ff0313f
Mubariz Afzal <mubarizafzal@gmail.com>
2019-08-21 13:30:08 -0400

    added dice and piece selection

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <FL/Fl_Button.H>
 #include <iostream>
 #include <FL/fl_draw.H>
+#include <cstdlib>
 
 
 /**
@@ -35,6 +36,10 @@ public:
 
   // 0 for yellow 3 for green
   int pieces [4][4];
+  int die1;
+  int die2;
+
+  int remaining;
 
   // -1 = 0, -9 = 8
 
@@ -102,6 +107,9 @@ public:
 
   int chosen = -1;
 
+  bool choosing = false;
+
+  int selection;
 
   /*
 
@@ -167,22 +175,79 @@ public:
           chosen = 3;
           std::cout << "Chosen Green" << "\n";
         }
+      } else {
+
+        // if haven't begun choosing, i.e. still clicking
+        if (!choosing) {
+
+
+          if (selected()) {
+
+            choosing = false;
+
+          }
+
+
+        } else {
+
+
+
+        }
+
+
       }
 
 
+    }
+
+  }
 
 
+  // return true if mouse over your own piece
+  bool selected () {
 
-      //std::cout << "does" << "\n";
+    int location;
+
+    int x, y;
+
+    int m_x, m_y;
+
+    for (int i = 0; i < 4; i++) {
+
+      location = pieces[chosen][i];
+      m_x = Fl::event_x();
+      m_y = Fl::event_y();
+
+      if (location < 0) {
+
+        location = (location*-1) -1;
+        x = base_squares[chosen][location*2];
+        y = base_squares[chosen][location*2 + 1];
+
+      } else {
+
+        x = square_cords[location][0];
+        y = square_cords[location][1];
+      }
+
+      if (m_x >= x-20 && m_x <= x+20 && m_y >= y-20 && m_y <= y+20) {
+
+        std::cout << "yes" << "\n";
+        return false;
+
+
+      }
 
 
 
     }
 
 
-
-
   }
+
+
+
+
 
   void draw () {
 
@@ -191,6 +256,16 @@ public:
     fl_rectf(this->x(), this->y(), this->w(), this->h(), FL_WHITE);
     draw_lines(this->x(), this->y());
     draw_rects(this->x(), this->y());
+
+    fl_draw ("You rolled:", 650, 360);
+    fl_draw(std::to_string(die1).c_str(), 680,400);
+    fl_draw(std::to_string(die2).c_str(), 720,400);
+
+    if (choosing) {
+      // draw choices
+
+
+    }
 
     //fl_color(FL_YELLOW);
     //draw_pieces(1);
@@ -202,62 +277,65 @@ public:
   }
 
   void draw_rects (int x, int y) {
-    fl_color(FL_YELLOW);
+
+    fl_color(237, 204, 19);
     fl_rectf(x, y + 360, 240, 240);
     fl_rectf(x + square_cords[0][0] - 15, y + square_cords[0][1] - 15, 30,30);
     fl_rectf(x + square_cords[47][0] - 15, y + square_cords[47][1] - 15, 30,30);
     draw_special(0);
-    draw_4(0, x, y + 360, FL_YELLOW);
+    draw_4(0, x, y + 360);
+    draw_pieces(0, 237, 204, 19);
 
-    fl_color(FL_BLUE);
+    fl_color(19, 117, 237);
     fl_rectf(x, y, 240, 240);
     fl_rectf(x + square_cords[13][0] - 15, y + square_cords[13][1] - 15, 30,30);
     fl_rectf(x + square_cords[8][0] - 15, y + square_cords[8][1] - 15, 30,30);
     draw_special(1);
-    draw_4(0, x, y, FL_BLUE);
+    draw_4(1, x, y);
+    draw_pieces(1, 19, 117, 237);
 
-    fl_color(FL_RED);
+    fl_color(237, 37, 19);
     fl_rectf(x + 360, y, 240, 240);
     fl_rectf(x + square_cords[26][0] - 15, y + square_cords[26][1] - 15, 30,30);
     fl_rectf(x + square_cords[21][0] - 15, y + square_cords[21][1] - 15, 30,30);
     draw_special(2);
-    draw_4(0, x + 360, y, FL_RED);
+    draw_4(2, x + 360, y);
+    draw_pieces(2, 237, 37, 19);
 
-    fl_color(FL_GREEN);
+
+    fl_color(67, 219, 33);
     fl_rectf(x + 360, y + 360, 240, 240);
     fl_rectf(x + square_cords[39][0] - 15, y + square_cords[39][1] - 15, 30,30);
     fl_rectf(x + square_cords[34][0] - 15, y + square_cords[34][1] - 15, 30,30);
     draw_special(3);
-    draw_4(0, x + 360, y + 360, FL_GREEN);
-
+    draw_4(3, x + 360, y + 360);
+    draw_pieces(3, 67, 219, 33);
 
 
   }
 
-  void draw_4 (int i, int x, int y, int color) {
+  void draw_4 (int i, int x, int y) {
 
 
 
     fl_color(FL_WHITE);
 
     fl_rectf(x + 60, y + 60, 40, 40);
-    Board::base_squares[i][0] = x + 60;
-    Board::base_squares[i][1] = y + 60;
+    base_squares[i][0] = x + 60 + 20;
+    base_squares[i][1] = y + 60 + 20;
 
     fl_rectf(x + 140, y + 60, 40, 40);
-    base_squares[i][2] = x + 140;
-    base_squares[i][3] = y + 60;
+    base_squares[i][2] = x + 140 + 20;
+    base_squares[i][3] = y + 60 + 20;
 
     fl_rectf(x + 60, y + 140, 40, 40);
-    base_squares[i][4] = x + 60;
-    base_squares[i][5] = y + 140;
+    base_squares[i][4] = x + 60 + 20;
+    base_squares[i][5] = y + 140 + 20;
 
     fl_rectf(x + 140, y + 140, 40, 40);
-    base_squares[i][6] = x + 140;
-    base_squares[i][7] = y + 140;
+    base_squares[i][6] = x + 140 + 20;
+    base_squares[i][7] = y + 140 + 20;
 
-    fl_color(color);
-    draw_pieces(i);
 
   }
 
@@ -316,7 +394,7 @@ public:
   }
 
 
-  void draw_pieces (int i) {
+  void draw_pieces (int i, int r, int g, int b) {
 
     int location;
     double x, y;
@@ -328,9 +406,8 @@ public:
       if (location < 0) {
         location = (location*-1) - 1;
 
-        std::cout << location << "\n";
-        x = (double)base_squares[i][location*2] + 20.0;
-        y = (double)base_squares[i][location*2 + 1] + 20.0;
+        x = (double)base_squares[i][location*2];
+        y = (double)base_squares[i][location*2 + 1];
 
       } else {
 
@@ -339,8 +416,12 @@ public:
 
       }
 
-      std::cout << x << "  " << y << "\n";
+      fl_color(r, g, b);
+      fl_circle (x, y, 16.0);
       fl_circle (x, y, 15.0);
+
+      fl_color(FL_BLACK);
+      fl_circle (x, y, 12.0);
 
     }
   }
@@ -348,24 +429,59 @@ public:
 
 };
 
+Fl_Window *window;
+Board *box;
+
+Fl_Button *roll_button;
+
+
+void roll_callback (Fl_Widget* widget, void*) {
+
+
+  box->die1 = (rand() % 6) + 1;
+  box->die2 = (rand() % 6) + 1;
+  window->redraw();
+
+
+  ((Fl_Button*)widget)->deactivate();
+
+}
+
+void next_callback (Fl_Widget* widget, void*) {
+
+  (roll_button)->activate();
+}
+
 
 
 int main (int argc, char ** argv)
 {
-  Fl_Window *window;
-  Board *box;
   Fl_Button *start_button;
-  Fl_Button *roll_button;
+
   Fl_Button *next_button;
   Fl_Box *m_board;
 
   window = new Fl_Window (800, 700);
+  window->color(fl_lighter(FL_GRAY));
 
   start_button = new Fl_Button (20,20,100,60);
+  start_button->label("New Game");
 
   roll_button = new Fl_Button (620,20,160,160);
+  roll_button->labelsize(20);
+  roll_button->labelfont(FL_HELVETICA_BOLD);
+  roll_button->label("ROLL DICE");
+  roll_button->callback(roll_callback, 0);
 
   next_button = new Fl_Button (620, 500, 160, 160);
+  next_button->labelsize(20);
+  next_button->labelfont(FL_HELVETICA_BOLD);
+  next_button->label("NEXT TURN");
+  next_button->callback(next_callback, 0);
+
+
+
+
 
   m_board = new Fl_Box(150, 20, 450, 60);
   m_board->box(FL_THIN_UP_BOX);
@@ -373,7 +489,6 @@ int main (int argc, char ** argv)
 
   box = new Board(0, 100, 600, 600);
   box->box(FL_THIN_UP_BOX);
-  box->color(FL_BLACK);
 
   window->end ();
   window->show (argc, argv);
